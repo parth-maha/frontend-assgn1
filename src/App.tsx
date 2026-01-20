c73eb70a5c68c55ef034342a3da485c440979ee5
parth-maha <parth.maha@roimaint.com>
2026-01-20 14:31:26 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-center bg-amber-100'>
+      Parth Maha
+    </div>
+  )
+}
+
+export default App
